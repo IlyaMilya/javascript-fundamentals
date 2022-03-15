db0196940d939e8b49c431712660eee618373cbc
Ilya <milyavskyfamily@gmail.com>
2022-03-15 15:31:33 -0400

    Addes js module file

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('JavaScript is running!')
+console.log('The JS Module is loaded!')
+let score = 100
+
+let h1 = document.createElement('h1')
+h1.innerText = 'This came from JavaScript'
+document.body.appendChild(h1)
+
