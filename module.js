1e99107d368f493dc1dba8004bf87d927a1a7b96
Ilya <milyavskyfamily@gmail.com>
2022-03-15 15:44:55 -0400

    added functions in module

--- module.js
+++ module.js
@@ -10,9 +10,14 @@ all of this functionality
 
 */
 
-let myName = prompt('Enter your name')
+
+
+function quiz() {
+    let myName = prompt('Enter your name')
 let month = prompt('What month were you born?')
 let food = prompt('What is your favorite food?')
 console.log('My name is', myName)
 console.log('I was born in', month)
 console.log('My favorite food is', food)
+}
+
