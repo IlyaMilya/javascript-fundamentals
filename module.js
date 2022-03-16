e9df3de2f7f45757afd30fe0cfa9a054c45b78e6
Ilya <milyavskyfamily@gmail.com>
2022-03-16 12:10:17 -0400

    functions overview

--- module.js
+++ module.js
@@ -10,14 +10,80 @@ all of this functionality
 
 */
 
+//const quiz = () => {
+    //let myName = prompt('Enter your name')
+    //let month = prompt('What month were you born?')
+    //let food = prompt('What is your favorite food?')
+    //console.log('My name is', myName)
+    //console.log('I was born in', month)
+    //console.log('My favorite food is', food)
+//}
 
 
-function quiz() {
+
+const quiz = () => {
     let myName = prompt('Enter your name')
-let month = prompt('What month were you born?')
-let food = prompt('What is your favorite food?')
-console.log('My name is', myName)
-console.log('I was born in', month)
-console.log('My favorite food is', food)
+    let month = prompt('What month were you born?')
+    let food = prompt('What is your favorite food?')
+    console.log('My name is', myName)
+    console.log('I was born in', month)
+    console.log('My favorite food is', food)
 }
 
+ const notifyA = () => {
+ console.log('This is', this)
+ }
+
+ const objectA = {
+     name: function() {
+         console.log
+     }
+ }
+
+ const objectB = {
+     name: () => {
+        console.log('This is')
+     }
+ }
+
+ //arrow syntax function refers to global scope 
+
+ /*
+ 1. Functions allow to specify exactly when we want to call al lof this functionality
+ 2. the different syntaxes change the lexical 
+
+ */
+
+ const print = () => {
+     console.log('I love to print text!')
+ }
+
+ const functionA = (f) => {
+     console.log('FUNCTION A HAS BEEN ACTIVATED!');
+     f()
+ }
+
+ const functionB = () => {
+     console.log('function B has been called!')
+ }
+
+ const greet =  (person) => {
+    console.log('HELLO!', person)
+ }
+
+ //parameter (person) in DOM just like in function 
+
+ const blender = (string) => {
+     return string.split('')
+ }
+
+ const square = (num, message) => {
+    console.log("message:", message) 
+    return num * num
+ }
+
+ //the order of your parameters matter when you reach them in your DOM********
+
+ const functionC = () => {
+     console.log('FUNTION C HAS BEEN CALLED!')
+ }
