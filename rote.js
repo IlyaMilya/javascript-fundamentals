ce61af4b1dc0f0da35dbb873cfd58eb628350550
Ilya <milyavskyfamily@gmail.com>
2022-03-16 13:31:31 -0400

    Spiderman

---
+++ rote.js
@@ -0,0 +1,16 @@
+const greet = () => 
+console.log('Hello world')
+
+greet()
+
+const square = (num) => {
+    return num * num
+}
+
+square(25)
+
+const greetByName = (str) => {
+    console.log('Hello', str)
+}
+
+greetByName('Spiderman')
